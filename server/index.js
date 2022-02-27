b9f874460269b9f695ac3b32f5c81b6c354651c1
ewan-kerboas <pro.ewank@gmail.com>
2022-02-27 22:09:40 +0100

    Create game

--- server/index.js
+++ server/index.js
@@ -10,73 +10,61 @@ const io = require("socket.io")(server, {
 const PORT = 4000;
 
 class Player {
-  constructor(id, username) {
+  constructor(id, username, socket) {
       this.id = id;
       this.username = username;
+      this.socket = socket;
   }
 }
+
+class Game {
+  constructor(id) {
+    this.id = id;
+    this.players = [];
+  }
+}
+
+let i = 1;
 
 //wating players
-let wps = []
+let wps = [];
 
-let lastPlayer = null;
+let games = [];
 
-function matchmake() {
-  // adding last player at the top of the pile
-  if(lastPlayer != null) wps = [lastPlayer, ...wps];
+function generateRandomNumber(numberOfCharacters) {
+  var randomValues = '';
+  var stringValues = 'ABCDEFGHIJKLMNOabcdefghijklmnopqrstuvwxyzPQRSTUVWXYZ';  
+  var sizeOfCharacter = stringValues.length;  
 
-  // save the last impaire player for the next matchmaking
-  if (wps.length % 2 == 1) {
-      console.log("impaire")
-      lastPlayer = wps[wps.length - 1];
-      wps.splice(wps.length - 1, 1);
+  for (var i = 0; i < numberOfCharacters; i++) {
+     randomValues = randomValues+stringValues.charAt(Math.floor(Math.random() * sizeOfCharacter));
   }
 
-  // matchmake
-  let matchs = []
-  const midLength = wps.length / 2;
+  return randomValues;
+} 
 
-  for (let x = 0; x < midLength; x++) {
-      const _rnd = dblRnd(wps.length-1);
-      matchs.push({ red: wps.splice(_rnd.red, 1)[0], blue: wps.splice(_rnd.blue, 1)[0] });
-  }
-
-  return matchs;
-}
-
-// random number
-function rnd(max) {
-  return Math.floor(Math.random() * max);
-}
-
-//double random number
-function dblRnd(max){
-  let red, blue;
-
-  red = rnd(max);
-  blue = rnd(max);
-
-  while (red != blue){
-      blue = rnd(max);
-  }
-
-  return {red, blue};
-}
-
-setInterval(() => {
-  if(wps.length > 1) {
-    const result = matchmake();
-    console.log(result);
-    
-    io.sockets.emit("initGame", result);
-    
-  }
-}, 1000)
 
 io.on('connection', socket => {
   socket.on("addPlayer", (obj) => {
-    wps.push(new Player(socket.id, obj.username));
-  })
+    if(wps.length == 0) {
+      wps.push(new Player(socket.id, obj.username, socket));
+    } else {
+      let randomId = generateRandomNumber(12);
+      console.log(randomId);
+
+      wps[0].socket.join(`game-${randomId}`);
+      socket.join(`game-${randomId}`);
+
+      let game = new Game(`game-${randomId}`);
+
+      game.players.push(wps[0]);
+      game.players.push(new Player(socket.id, obj.username, socket));
+
+      games.push(game);
+
+      console.log(games);
+    }
+  });
 
   socket.on("removePlayer", (obj) => {
     const removeIndex = wps.findIndex(item => item.id === obj.id);
