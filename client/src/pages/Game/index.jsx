13141f3e52389047b91c3a035f0f05ed87e95048
Ewan <ewan.kerboas@ezcorp.io>
2022-03-10 11:35:34 +0100

    Init game

--- client/src/pages/Game/index.jsx
+++ client/src/pages/Game/index.jsx
@@ -1,30 +1,28 @@
 import React, {useEffect, useState} from "react";
 import io from 'socket.io-client';
 import { useParams } from 'react-router-dom';
+import socket from "../../Socket.js";
 
 export function Game() {
-
   let { gameId } = useParams();
 
   const [test, setTest] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [adversaire, setAdversaire] = useState();
   
   useEffect(() => {
-    console.log('coucou')
     // Send the id of the game
     socket.emit("gameInit", gameId);
 
-    socket.on("hello", (obj) => {
-      console.log('testttt')
-      console.log(obj)
+    socket.on("gameStart", (obj) => {
+      setPlayers(obj)
     })
   })
 
   return (
     <>
-      <h1>Into game ! {
-        test &&
-        test
-      }</h1>
+      <h1>Into game !</h1>
+      {adversaire}
     </>
   );
 };
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,27 +1,118 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
-import io from 'socket.io-client';
+import socket from "../../Socket";
+import Web3 from "web3";
 
 import "./Home.css";
-
-const socket = io.connect('http://localhost:4000');
 
 export function Home() {
 
   const [inMatchmaking, setInMatchmaking] = React.useState(false);
+  const [pseudo, setPseudo] = React.useState(" ");
+  const [isConnected, setIsConnected] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
+  const [supply, setSupply] = useState(); 
 
   const navigate = useNavigate();
 
+  useEffect(() => {
+    function checkConnectedWallet() {
+      const userData = JSON.parse(localStorage.getItem('userAccount'));
+      if (userData != null) {
+        setUserInfo(userData);
+        setIsConnected(true);
+      }
+    }
+    checkConnectedWallet();
+
+    const currentProvider = detectCurrentProvider();
+    if(currentProvider) {
+      const web3js = new Web3(currentProvider);
+        const contract = new web3js.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+
+        async function getSupplyFunction() {
+          contract.methods.getSupply().call().then((res) => {
+            setSupply(res)
+          })
+        }
+  
+        getSupplyFunction();
+    }
+
+  }, []);
+
+  const detectCurrentProvider = () => {
+    let provider;
+    if (window.ethereum) {
+      provider = window.ethereum;
+    } else if (window.web3) {
+      // eslint-disable-next-line
+      provider = window.web3.currentProvider;
+    } else {
+      toastError({
+        title: 'Non-Ethereum browser detected. You should consider trying MetaMask!',
+        description: '',
+        status: 'error',
+        duration: 4000,
+        isClosable: true,
+      })
+    }
+    return provider;
+  };
+
+  const onConnect = async () => {
+    try {
+      const currentProvider = detectCurrentProvider();
+      if (currentProvider) {
+        if (currentProvider !== window.ethereum) {
+          console.log('Non ethereum browser detected')
+        }
+        await currentProvider.request({ method: 'eth_requestAccounts' });
+        const web3 = new Web3(currentProvider);
+        const userAccount = await web3.eth.getAccounts();
+        const chainId = await web3.eth.getChainId();
+        if(chainId !== 43113) return alert("you must connect to avalanche ");
+        const account = userAccount[0];
+        let ethBalance = await web3.eth.getBalance(account); // Get wallet balance
+        ethBalance = web3.utils.fromWei(ethBalance, 'ether'); //Convert balance to wei
+        saveUserInfo(ethBalance, account, chainId);
+        if (userAccount.length === 0) {
+          console.log("please connect to metamask")
+        }
+      }
+    } catch (err) {
+      console.log("There was an error fetching your accounts. Make sure your Ethereum client is configured correctly.")
+    }
+  };
+
+  const onDisconnect = () => {
+    window.localStorage.removeItem('userAccount');
+    setUserInfo({});
+    setIsConnected(false);
+  };
+
+  const saveUserInfo = (ethBalance, account, chainId) => {
+    const userAccount = {
+      account: account,
+      balance: ethBalance,
+      connectionid: chainId,
+    };
+    window.localStorage.setItem('userAccount', JSON.stringify(userAccount)); //user persisted data
+    const userData = JSON.parse(localStorage.getItem('userAccount'));
+    setUserInfo(userData);
+    setIsConnected(true);
+  };
+
   socket.on('isInGame', (obj) => {
     if(obj.is == true) {
-      navigate(`game-${obj.id}`, {socket: socket})
+      navigate(`game-${obj.id}`)
     }
   })
 
   function joinMatchmaking() {
     setInMatchmaking(true)
 
-    socket.emit("addPlayer", {username: "testCoco"})
+    socket.emit("addPlayer", {username: pseudo})
   }
 
   function leaveMatchmaking() {
@@ -30,8 +121,13 @@ export function Home() {
     socket.emit("removePlayer", {id: socket.id})
   }
 
+  const handleChange = (e) => {
+    setPseudo(e.target.value);
+  }
+
   return (
     <div className="home-container">
+        <input onChange={handleChange} />
         <button onClick={joinMatchmaking} className="enter-room-button">Play</button>
         {
           inMatchmaking &&
